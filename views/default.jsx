a40b23fbe52a8e28dc6b4a1dc62541df8132d09f
Neel Patel <neelpatel@Neels-MacBook-Pro.local>
2022-08-11 20:18:56 -0400

    Adding styles to REST-Rant

--- views/default.jsx
+++ views/default.jsx
@@ -5,6 +5,13 @@ function Def(html) {
     <html>
       <head>
         <title>Title</title>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+          integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N"
+          crossorigin="anonymous"
+        ></link>
+        <link rel="stylesheet" href="/css/style.css"></link>
       </head>
       <body>{html.children}</body>
     </html>
