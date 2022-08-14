726a52b6b732a4758232138af6572b879e7302ad
Neel Patel <neelpatel@Neels-MacBook-Pro.local>
2022-08-14 11:11:53 -0400

    Navbar with style

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -7,7 +7,7 @@ function index(data) {
       <div className="col-sm-6">
         <h2>{place.name}</h2>
         <p className="text-center">{place.cuisines}</p>
-        <img src={place.pic} alt={place.name} />
+        <img className="myPic" src={place.pic} alt={place.name} />
         <p className="text-center">
           Located in {place.city}, {place.state}
         </p>
