b96e351896a8da5d80244bf84e5d803b31cd2f34
Neel Patel <neelpatel@Neels-MacBook-Pro.local>
2022-08-14 14:10:20 -0400

    Edit functionality

--- controllers/places.js
+++ controllers/places.js
@@ -4,6 +4,31 @@ const places = require("../models/places.js");
 router.get("/new", (req, res) => {
   res.render("places/new");
 });
+// edit route
+router.put("/:id", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    // Dig into req.body and make sure data is valid
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = "http://placekitten.com/400/400";
+    }
+    if (!req.body.city) {
+      req.body.city = "Anytown";
+    }
+    if (!req.body.state) {
+      req.body.state = "USA";
+    }
+
+    // Save the new data into places[id]
+    places[id] = req.body;
+    res.redirect(`/places/${id}`);
+  }
+});
 
 router.get("/:id", (req, res) => {
   let id = Number(req.params.id);
@@ -15,6 +40,19 @@ router.get("/:id", (req, res) => {
     res.render("places/show", { place: places[id], id });
   }
 });
+
+//edit place
+router.get("/:id/edit", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    res.render("places/edit", { place: places[id], index: id });
+  }
+});
+
 //delete place
 router.delete("/:id", (req, res) => {
   let id = Number(req.params.id);
