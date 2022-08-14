49419ad503f538c597018f2454d7a71b96fa796f
Neel Patel <neelpatel@Neels-MacBook-Pro.local>
2022-08-14 12:49:19 -0400

    Delete function working

--- controllers/places.js
+++ controllers/places.js
@@ -1,15 +1,38 @@
 const router = require("express").Router();
 const places = require("../models/places.js");
-
+// add new places
 router.get("/new", (req, res) => {
   res.render("places/new");
+});
+
+router.get("/:id", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    res.render("places/show", { place: places[id], id });
+  }
+});
+//delete place
+router.delete("/:id", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    places.splice(id, 1);
+    res.redirect("/places");
+  }
 });
 
 // GET /places
 router.get("/", (req, res) => {
   res.render("places/index", { places });
 });
-
+// post places
 router.post("/", (req, res) => {
   if (!req.body.pic) {
     // Default image if one is not provided
