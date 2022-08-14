3acdb6ea803997cfcb4b76970ca53e75a9b90246
Neel Patel <neelpatel@Neels-MacBook-Pro.local>
2022-08-14 09:58:29 -0400

    New places can be added

---
+++ models/places.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    name: "H-Cafe-ML",
+    city: "Seattle",
+    state: "WA",
+    cuisines: "American, Brunch",
+    pic: "/images/thai.jpg",
+    id: "thai",
+  },
+  {
+    name: "Coding Bar",
+    city: "Phoenix",
+    state: "AZ",
+    cuisines: "Alcohol, Drinks",
+    pic: "/images/dog.jpg",
+    id: "bar",
+  },
+];
