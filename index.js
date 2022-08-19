7696cac3e362ddb9186d485d97e74edc284b55c5
Neel Patel <neelpatel@Neels-MacBook-Pro.local>
2022-08-18 20:01:41 -0400

    Mongoose ODM implemented

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const methodOverride = require("method-override");
+const mongoose = require("mongoose");
 
 // Express Settings
 app.set("views", __dirname + "/views");
@@ -11,6 +12,15 @@ app.engine("jsx", require("express-react-views").createEngine());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+
+mongoose.connect(
+  process.env.MONGO_URI,
+  { family: 4 },
+  function (err, connection) {
+    // connection.db('breads');
+    console.log("connected to DB!!");
+  }
+);
 
 // Controllers & Routes
 app.use("/places", require("./controllers/places"));
@@ -22,6 +32,7 @@ app.get("/", (req, res) => {
 app.get("*", (req, res) => {
   res.render("error404");
 });
-
 // Listen for Connections
-app.listen(process.env.PORT);
+app.listen(process.env.PORT, function () {
+  console.log("Server is alive!");
+});
--- models/places.js
+++ models/places.js
@@ -1,18 +1,31 @@
-module.exports = [
-  {
-    name: "H-Cafe-ML",
-    city: "Seattle",
-    state: "WA",
-    cuisines: "American, Brunch",
-    pic: "/images/thai.jpg",
-    id: "thai",
-  },
-  {
-    name: "Coding Bar",
-    city: "Phoenix",
-    state: "AZ",
-    cuisines: "Alcohol, Drinks",
-    pic: "/images/dog.jpg",
-    id: "bar",
-  },
-];
+const mongoose = require("mongoose");
+
+const placeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  pic: String,
+  cuisines: { type: String, required: true },
+  city: { type: String, default: "Anytown" },
+  state: { type: String, default: "USA" },
+  founded: Number,
+});
+
+module.exports = mongoose.model("Place", placeSchema);
+
+// module.exports = [
+//   {
+//     name: "H-Cafe-ML",
+//     city: "Seattle",
+//     state: "WA",
+//     cuisines: "American, Brunch",
+//     pic: "/images/thai.jpg",
+//     id: "thai",
+//   },
+//   {
+//     name: "Coding Bar",
+//     city: "Phoenix",
+//     state: "AZ",
+//     cuisines: "Alcohol, Drinks",
+//     pic: "/images/dog.jpg",
+//     id: "bar",
+//   },
+// ];
